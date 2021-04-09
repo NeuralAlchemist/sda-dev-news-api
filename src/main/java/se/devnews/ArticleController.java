ee3c453ec5dfbd86de91ffc90e599d67ed9b386d
Yash Seeta <yashseeta94@gmail.com>
2021-04-09 16:57:24 +0200

    Add put mapping to update article

--- src/main/java/se/devnews/ArticleController.java
+++ src/main/java/se/devnews/ArticleController.java
@@ -1,5 +1,6 @@
 package se.devnews;
 
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,5 +34,13 @@ public class ArticleController {
     public ResponseEntity<Article> getArticleById(@PathVariable Long id){
         Article foundArticle = articleRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
         return ResponseEntity.ok(foundArticle);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable Long id, @RequestBody Article updatedArticle){
+        articleRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        updatedArticle.setId(id);
+        Article article = articleRepository.save(updatedArticle);
+        return ResponseEntity.ok(article);
     }
 }
