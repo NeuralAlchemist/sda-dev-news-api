e2d118db74e732c7cd047e16e32261d46f9c837f
Yash Seeta <yashseeta94@gmail.com>
2021-04-09 15:50:25 +0200

    add mapping to get all articles/ by id and create an article

--- src/main/java/se/devnews/ArticleController.java
+++ src/main/java/se/devnews/ArticleController.java
@@ -1,15 +1,37 @@
 package se.devnews;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/articles")
 @RestController
 public class ArticleController {
 
-    @RequestMapping
-    public String index(){
-        return "Welcome to Exercise 1";
+    ArticleRepository articleRepository;
+    @Autowired
+    public ArticleController(ArticleRepository articleRepository){
+        this.articleRepository = articleRepository;
     }
 
+    @GetMapping
+    public ResponseEntity<List<Article>> listAllArticles(){
+        List<Article> articles = articleRepository.findAll();
+        return ResponseEntity.ok(articles);
+    }
+
+    @PostMapping
+    public ResponseEntity<Article> createArticle(@RequestBody Article article){
+        articleRepository.save(article);
+        return ResponseEntity.status(HttpStatus.CREATED).body(article);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Article> getArticleById(@PathVariable Long id){
+        Article foundArticle = articleRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        return ResponseEntity.ok(foundArticle);
+    }
 }
