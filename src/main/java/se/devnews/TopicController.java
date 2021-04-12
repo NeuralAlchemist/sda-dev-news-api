a837596efd184ee101004ce72fe7d89c6730b408
Yash Seeta <yashseeta94@gmail.com>
2021-04-12 21:06:46 +0200

    Add mappings to create a topic and associate an article with a topic (may or maynot exist)

--- src/main/java/se/devnews/TopicController.java
+++ src/main/java/se/devnews/TopicController.java
@@ -1,2 +1,43 @@
-package se.devnews;public class TopicController {
+package se.devnews;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class TopicController {
+
+    TopicRepository topicRepository;
+    ArticleRepository articleRepository;
+
+    public TopicController(TopicRepository topicRepository, ArticleRepository articleRepository) {
+        this.topicRepository = topicRepository;
+        this.articleRepository = articleRepository;
+    }
+
+    // Create a new topic
+    @PostMapping("/topics")
+    public ResponseEntity<Topic> createTopic(@Valid @RequestBody Topic topic){
+        topicRepository.save(topic);
+        return ResponseEntity.status(HttpStatus.CREATED).body(topic);
+    }
+
+    // associate the topic with the article given by articleId. If topic does not already exist, it is created.
+    @PostMapping("/articles/{articleId}/topics")
+    public ResponseEntity<Article> addArticleToTopic(@RequestBody Topic topicToAdd, @PathVariable Long articleId){
+        Article article = articleRepository.findById(articleId).orElseThrow(ResourceNotFoundException::new);
+        boolean topicExists = topicRepository.existsByName(topicToAdd.getName());
+        if (!topicExists){
+            topicRepository.save(topicToAdd);
+        } else {
+            topicToAdd = topicRepository.findByName(topicToAdd.getName());
+        }
+        article.getTopicsList().add(topicToAdd);
+        articleRepository.save(article);
+        return ResponseEntity.status(HttpStatus.CREATED).body(article);
+    }
 }
