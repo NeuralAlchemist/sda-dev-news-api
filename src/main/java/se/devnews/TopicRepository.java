d7e7c5c3c355da04a3da4d9c6c28924e681a54a5
Yash Seeta <yashseeta94@gmail.com>
2021-04-12 21:09:07 +0200

    Added derived query method for existsbyid and findbyname

--- src/main/java/se/devnews/TopicRepository.java
+++ src/main/java/se/devnews/TopicRepository.java
@@ -1,2 +1,9 @@
-package se.devnews;public interface TopicRepository {
+package se.devnews;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicRepository extends JpaRepository<Topic, Long> {
+    Topic findByName(String name);
+    boolean existsByName(String name);
 }
+/**/
