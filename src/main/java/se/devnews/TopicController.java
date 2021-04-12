b6ffa499be26ca2610fa9332bed0c86f6b06c60b
Yash Seeta <yashseeta94@gmail.com>
2021-04-12 21:23:28 +0200

    Add mapping to get all topics of an article

--- src/main/java/se/devnews/TopicController.java
+++ src/main/java/se/devnews/TopicController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
@@ -46,5 +47,13 @@ public class TopicController {
     public ResponseEntity<List<Topic>> getAllTopics(){
         List<Topic> allTopics = topicRepository.findAll();
         return ResponseEntity.ok(allTopics);
+    }
+
+    // return all topics associated with article given by `articleId`
+    @GetMapping("/articles/{articleId}/topics")
+    public ResponseEntity<Set<Topic>> getAllTopicsOfArticle(@PathVariable Long articleId){
+        Article givenArticle = articleRepository.findById(articleId).orElseThrow(ResourceNotFoundException::new);
+        Set<Topic> allTopicOfGivenArticle = givenArticle.getTopicsList();
+        return ResponseEntity.ok(allTopicOfGivenArticle);
     }
 }
